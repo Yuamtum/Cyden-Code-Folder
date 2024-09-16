a8985c815e3a2b47b5ec16558d4b0a4e12b97987
Yuamtum <dusarancyden@gmail.com>
2024-09-16 08:53:20 -0600

    Fridays edits

---
+++ Lesson4Demo/src/App.java
@@ -0,0 +1,17 @@
+// Project: lesson 4 demo 
+// My name is Cyden 
+// Date: 13 Sept 2024
+//Description: Going over comments and special characters
+/*Mayn what am I even doing */
+
+
+
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("How was your day going");
+        System.out.println(" It's cloudy with a chance of meat balls today");
+    }
+}
