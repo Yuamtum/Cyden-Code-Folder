2c3d975dc0057b94dc17fa5bd2aa4b5ba544c2f5
Yuamtum <dusarancyden@gmail.com>
2024-09-16 09:48:46 -0600

    The mona lisa art-Cyden

---
+++ Mona LIsa/src/App.java
@@ -0,0 +1,25 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("                         --------------------------");
+        System.out.println("                      ---                          ---");
+        System.out.println("                   ---           / /                 ---  ")
+        System.out.println("                --- ____________/ /______________      --- ")
+        System.out.println("")
+        System.out.println("")
+        System.out.println("")
+        System.out.println("")
+        System.out.println("")
+        System.out.println("")
+        System.out.println("")
+        System.out.println("")
+        System.out.println("")
+        System.out.println("")
+        System.out.println("")
+        System.out.println("")
+        System.out.println("")
+        System.out.println("")
+        System.out.println("")
+        System.out.println("")
+    }
+}  
