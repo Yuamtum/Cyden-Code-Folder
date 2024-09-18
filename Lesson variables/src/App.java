94b9354db9157703b98c526cc3f99cf80a7ba744
Yuamtum <dusarancyden@gmail.com>
2024-09-18 08:54:28 -0600

    Tuesdays work

---
+++ Lesson variables/src/App.java
@@ -0,0 +1,16 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+    //     System.out.println(1/3);
+    //     System.out.println(1.0/3);
+    //     System.out.println(1/3.0);
+    //     System.out.println(1.0/3.0);
+
+    //     int myNum = 2147483647;
+    //     System.out.println(myNum);
+    //     myNum = myNum + 1;
+    //     System.out.println(myNum);
+    // 
+
+     Double num = Math.random();
+     System.out.println(num);
+}
--- Mona LIsa/src/App.java
+++ Mona LIsa/src/App.java
@@ -3,23 +3,33 @@ public class App {
         System.out.println("Hello, World!");
         System.out.println("                         --------------------------");
         System.out.println("                      ---                          ---");
-        System.out.println("                   ---           / /                 ---  ")
-        System.out.println("                --- ____________/ /______________      --- ")
-        System.out.println("")
-        System.out.println("")
-        System.out.println("")
-        System.out.println("")
-        System.out.println("")
-        System.out.println("")
-        System.out.println("")
-        System.out.println("")
-        System.out.println("")
-        System.out.println("")
-        System.out.println("")
-        System.out.println("")
-        System.out.println("")
-        System.out.println("")
-        System.out.println("")
-        System.out.println("")
-    }
+        System.out.println("                   ---           / /                 ---  ");
+        System.out.println("                --- ____________/ /______________      --- ");
+        System.out.println("              /   /                              \         \ ");
+        System.out.println("             /   /                                \         \");
+        System.out.println("            /    |                                |          \");
+        System.out.println("           /     |                                |           \");
+        System.out.println("          |      |   ________     _________       |            |");
+        System.out.println("         |       |     __( 0)\   /  (0 )___       |             |");
+        System.out.println("        |        |            | |                 |              |");
+        System.out.println("        |        |            | |                 |              |   ");
+        System.out.println("        |        |            \_/                 |              |");
+        System.out.println("        |        |                                |              |");
+        System.out.println("        |        |                                |              |");
+        System.out.println("        |         \        (---\/---)            /               |");
+        System.out.println("        |          \                            /                |");
+        System.out.println("        |           \                          /                 |");
+        System.out.println("        |            \                        /                  |");
+        System.out.println("                       - - - - - - - - - - - -");
+        System.out.println("                              |              | ");
+        System.out.println("                               |              |");
+        System.out.println("                                |              |");
+        System.out.println("                               /                \");
+        System.out.println("                             /                    \");
+        System.out.println("                           /                        \");
+        System.out.println("                          |                          |");
+        System.out.println("                         |                            |");
+        System.out.println("                        |______                  ______|      ");
+        System.out.println("                               ------------------");
+        
 }  
