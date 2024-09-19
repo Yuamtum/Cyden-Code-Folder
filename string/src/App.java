66db18ede8f1d4461b4b30db6072fa072b126f45
Yuamtum <dusarancyden@gmail.com>
2024-09-19 09:48:12 -0600

    end of sept19

---
+++ string/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String message1 = "This is a test";
+        String message2 = "Hello class";
+
+
+        System.out.println(message1.length());
+        System.out.println(message2.length());
+    }
+}
