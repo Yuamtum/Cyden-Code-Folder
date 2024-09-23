c2b6ac8cae689e0c27ee2474d005be19e9da72b6
Yuamtum <dusarancyden@gmail.com>
2024-09-23 08:50:13 -0600

    Friday sept 20

--- Chicken wings/src/App.java
+++ Chicken wings/src/App.java
@@ -4,15 +4,13 @@
 //Cyden DUsaran
 
 
-
-
-
 public class App {
     public static void main(String[] args) throws Exception {
        Double Mynum = 4.75;
-       Double chi = Mynum / 0.75;
-       int tot = (int)chi;
-       System.out.println(tot);
+       Double cost = 0.75;
+       Double chi = Mynum/cost;
+       System.out.println(chi);
 
     
+}
 }
