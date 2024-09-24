5ece015f9247e7db03a3302c532136819869e293
Yuamtum <dusarancyden@gmail.com>
2024-09-24 09:36:53 -0600

    string replace-Sept 24, 2024

--- Strings replace/src/App.java
+++ Strings replace/src/App.java
@@ -1,5 +1,34 @@
+// String replace
+// Cyden Dusaran
+// Sept 19, 2024
+
+
+
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String message1 = " I am very happy! ";
+        String target1 = "very ";
+        System.out.println(message1);
+        System.out.println(message1.indexOf(target1));
+        System.out.println(message1.length());
+        System.out.print(message1.substring(0,6));
+        System.out.println(message1.substring(11,18));
+
+        // Part two of string replace
+        String message2 = "That was great - lol.";
+        String target2 = "lol";
+        String message3 = "laugh out loud";
+        System.out.println(message2);
+        System.out.println(message2.indexOf(target2));
+        System.out.println(message2.length());
+        System.out.print(message2.substring(0,17));
+        System.out.println(message3);
+
+
+    
+
+
+
     }
 }
