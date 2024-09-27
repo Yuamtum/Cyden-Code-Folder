ba4d5b48960fe3fa6e081585824221fa3e24d648
Yuamtum <dusarancyden@gmail.com>
2024-09-27 09:49:05 -0600

    sept 27 2024

---
+++ two class assignment/src/Secondclass.java
@@ -0,0 +1,12 @@
+public class Secondclass {
+public void speak() {
+    System.out.println("Who made this mess");
+
+}
+
+
+public void sayName() {
+    System.out.println("you did king");
+    speak();
+}
+}
