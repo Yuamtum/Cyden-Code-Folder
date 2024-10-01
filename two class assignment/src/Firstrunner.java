276dad27d37ff80949d4f8bf55f0a7d5851e6db1
Yuamtum <dusarancyden@gmail.com>
2024-10-01 09:43:34 -0600

    Oct 1 second class

---
+++ two class assignment/src/Firstrunner.java
@@ -0,0 +1,14 @@
+
+public class Firstrunner {
+    public static void main(String[] args) throws Exception {
+     Whodithat king = new Whodithat();
+
+    
+     
+     king.speak();
+     king.sayName();
+     king.sayName();
+
+
+    }
+}
---
+++ two class assignment/src/Whodithat.java
@@ -0,0 +1,16 @@
+
+public class Whodithat {
+    public static void speak() {
+        System.out.println("Who made this mess");
+    }
+
+
+    public void sayName(){
+
+    System.out.println("You did king");
+    speak();
+
+
+    }
+}
+
